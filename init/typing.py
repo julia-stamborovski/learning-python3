1962e427cd1be7a9645af4d4a1bbbac8c7641406
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-22 21:21:49 -0300

    typing
    Não há declaração de variáveis estáticas em Python, como em linguagens como C, Java ou C#. Nesas linguagens, indicamos o tipo e o nome das variáveis e ela já passa a existir. A variável em Python só passa a existir quando atribuímos um valor, já que não temos uma declaração expliícita do tipo. O interpretador do Python não tem como assumir um tipo.

---
+++ init/typing.py
@@ -0,0 +1,10 @@
+## Tipagem dinâmica
+food = "lasagne"
+type(food)
+food = 11
+type(food)
+# Exercício Alura "Qual o tipo da variável"
+preco = 49.99
+#Qual será o tipo da variável preco? 
+# Resposta: float 
+
