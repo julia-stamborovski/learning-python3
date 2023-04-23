5cf654917b6cdd4770350f2c21421ee8897c50d9
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-22 21:11:24 -0300

    init / functions-variables

---
+++ init/function_print_variables.py
@@ -0,0 +1,10 @@
+# comando print
+print("print", "do", "python", sep="-")
+print("print", "do", "python", sep="-", end="\n")
+print("print", "do", "python", sep="-", end="END")
+#definindo variaveis
+language = "python"
+content = "print do"
+#fim variaveis
+print(type(language))
+print(content, language)
