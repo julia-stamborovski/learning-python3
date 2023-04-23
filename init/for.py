31ce75a199ea649d161ba7cb08567a8fcff474f6
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-23 15:18:48 -0300

    for and rename file

---
+++ init/for.py
@@ -0,0 +1,28 @@
+print("welcome to the project guessing.py!")
+
+number = 10
+guessing_round = 1
+total_try = 3
+
+
+for guessing_round in range(1,3):
+    print(f"round: {guessing_round} of {total_try}")
+    
+    number_input = input("type a number: ")
+    print("you type the number:", number_input)
+    number_input = int(number_input)
+    victory = number_input == number
+    greater = number_input > number
+    less = number_input < number
+    
+    if victory:
+        print("victory!")
+    else:
+        if greater:
+            print("defeat!, your number is greater than the misterious number...")
+        elif less:
+            print("defeat!, your number is less than the misterious number...")
+
+    guessing_round += 1
+print("end")
+
